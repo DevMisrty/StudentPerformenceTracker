93f075cee39ca317d8cb09a8152f3fe94ed6e714
dev Mistry <dev.mistry.agileinfoways@gmail.com>
2025-07-17 15:29:09 +0530

    startingProject

---
+++ src/main/java/com/Assessment/StudentPerformanceTracker/StudentPerformanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.Assessment.StudentPerformanceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPerformanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Assessment/StudentPerformanceTracker/StudentPerformanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assessment.StudentPerformanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPerformanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
